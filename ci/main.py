90ff35e548e10b748cbe13d26f6b8b055f0f6311
Jorge Cañardo Alastuey <jorgecanardo@gmail.com>
2015-10-26 21:42:21 -0600

    BUG: input to all must be iterable

--- ci/main.py
+++ ci/main.py
@@ -47,9 +47,9 @@ def is_already_uploaded(name, version, build_number):
     print('CHECKING COMMAND: {0}'.format(check_cmd))
     out = check_output(check_cmd, shell=True)
     res = json.loads(out)
-    return all(name in res,
-               res[name][0]['version'] == version,
-               res[name][0]['build_number'] == build_number)
+    return all((name in res,
+                res[name][0]['version'] == version,
+                res[name][0]['build_number'] == build_number))
 
 
 def upload(name, version):
