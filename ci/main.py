466cee33ad44a5d75ee49d074a3177cc32a42bc8
Zech Xu <zhenjiang.xu@gmail.com>
2015-11-06 00:48:09 -0800

    Replace is_already_uploaded with is_not_uploaded
    The code is clearer with the fix for version and build number checker.

--- ci/main.py
+++ ci/main.py
@@ -41,15 +41,16 @@ def build_upload_recipes(p, channel):
                 except KeyError:
                     # Build number is 0 if not specified
                     build_number = 0
-                if is_already_uploaded(name, version, build_number, channel):
-                    # Only new packages (either version or build_number)
-                    log.info("Skipping package: {0}-{1}".format(name, version))
-                    continue
-                build(root)
-                if os.environ['TRAVIS_SECURE_ENV_VARS'] == 'true':
-                    upload(name, version, channel)
+                if is_not_uploaded(name, version, build_number, channel):
+                    build(root)
+                    if os.environ['TRAVIS_SECURE_ENV_VARS'] == 'true':
+                        upload(name, version, channel)
+                    else:
+                        log.info("Uploading not available in Pull Requests")
                 else:
-                    log.info("Uploading not available in Pull Requests")
+                    # Only new packages (either version or build_number)
+                    log.info("Skipping package: {0}-{1}-{2}".format(
+                        name, version, build_number))
 
 
 def build(root):
@@ -66,7 +67,7 @@ def build(root):
     check_call(build_cmd, shell=True)
 
 
-def is_already_uploaded(name, version, build_number, channel):
+def is_not_uploaded(name, version, build_number, channel):
     '''Check if we want to build & upload a package.
 
     It checks package name, version and build number
@@ -88,7 +89,7 @@ def is_already_uploaded(name, version, build_number, channel):
     Bool
         If a package in the channel has the same name, the same
         version, and an equal or higher build number, then return
-        True; otherwise, return False.
+        False; otherwise, return True.
 
     '''
     check_cmd = ('conda search --json --override-channels '
@@ -97,10 +98,16 @@ def is_already_uploaded(name, version, build_number, channel):
     log.info('Checking: {0}'.format(check_cmd))
     out = check_output(check_cmd, shell=True)
     res = json.loads(out)
-    build_number_remote = max(i['build_number'] for i in res['minced'])
-    return (name in res and
-            res[name][0]['version'] == version and
-            build_number_remote >= build_number)
+
+    if name not in res:
+        return True
+    pkg = res[name]
+    if version not in [i['version'] for i in pkg]:
+        return True
+    if build_number > max(
+            i['build_number'] for i in pkg if i['version'] == version):
+        return True
+    return False
 
 
 def upload(name, version, channel):
@@ -119,14 +126,11 @@ def upload(name, version, channel):
         config.bldpkgs_dir,
         '{0}-{1}*.tar.bz2'.format(name, version))
     built = glob.glob(built_glob)[0]
-    upload_cmd = 'anaconda -t {token} upload -u {channel} {built}'
+    upload_cmd = 'anaconda -t {token} upload -u %s %s' % (channel, built)
     # Do not show decrypted token!
     log.info('Uploading: {0}'.format(upload_cmd))
     check_call(
-        upload_cmd.format(
-            token=os.environ['ANACONDA_TOKEN'],
-            built=built,
-            channel=channel),
+        upload_cmd.format(token=os.environ['ANACONDA_TOKEN']),
         shell=True)
 
 
