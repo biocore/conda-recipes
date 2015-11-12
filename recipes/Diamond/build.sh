595a6ecaefc553409faad2ff0e42b5207193c1a6
Zech Xu <zhenjiang.xu@gmail.com>
2015-11-12 09:44:56 -0800

    Modify with help from Patrick

--- recipes/Diamond/build.sh
+++ recipes/Diamond/build.sh
@@ -4,6 +4,17 @@ conda install boost
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX/bin ..
+
+CMAKE_ARCH="-m"$ARCH
+INCLUDE_PATH=${PREFIX}/include
+LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DBOOST_ROOT=$PREFIX \
+      -DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
+      -DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
+      ..
+
 make install
 
