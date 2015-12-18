ac858a181ef40a719707068f883d457aad613d70
Zech Xu <zhenjiang.xu@gmail.com>
2015-12-17 18:52:42 -0800

    Fix Diamond
    The current build won't run well in Mac, as it is compiled with GNU gcc
    which is not readily available on OSX

--- recipes/Diamond/build.sh
+++ recipes/Diamond/build.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-mkdir build
-cd build
+case "$(uname)" in
+    Linux)
+        mkdir build
+        cd build
 
-CMAKE_ARCH="-m"$ARCH
-INCLUDE_PATH=${PREFIX}/include
-LIBRARY_PATH=${PREFIX}/lib
+        CMAKE_ARCH="-m"$ARCH
+        INCLUDE_PATH=${PREFIX}/include
+        LIBRARY_PATH=${PREFIX}/lib
 
-export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
+        export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
 
-cmake -LAH .. \
--DCMAKE_CXX_COMPILER=g++-4.8 \
--DCMAKE_C_COMPILER=gcc-4.8 \
--DCMAKE_PREFIX_PATH=$PREFIX \
--DCMAKE_INSTALL_PREFIX=$PREFIX \
--DBOOST_ROOT=$PREFIX \
--DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
--DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
+        cmake -LAH .. \
+              -DCMAKE_CXX_COMPILER=g++-4.8 \
+              -DCMAKE_C_COMPILER=gcc-4.8 \
+              -DCMAKE_PREFIX_PATH=$PREFIX \
+              -DCMAKE_INSTALL_PREFIX=$PREFIX \
+              -DBOOST_ROOT=$PREFIX \
+              -DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
+              -DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
 
-cmake --build . --config Release --target install
+              cmake --build . --config Release --target install
+        ;;
+    Darwin)
+        cd src
+        ./install-boost &> /dev/null
+        #sed -i "" "s/-march=native/ /g" Makefile
+        make
+        mkdir -p $PREFIX/bin
+        cp ../bin/diamond $PREFIX/bin
+        ;;
+esac
