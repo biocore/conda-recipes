994e41c6b9976013325d6a3928e116e09697f536
Zech Xu <zhenjiang.xu@gmail.com>
2015-12-18 15:07:53 -0800

    Oh well, just download executable

--- recipes/Diamond/build.sh
+++ recipes/Diamond/build.sh
@@ -2,23 +2,9 @@
 
 case "$(uname)" in
     Linux)
-        mkdir build
-        cd build
-
-        CMAKE_ARCH="-m"$ARCH
-        INCLUDE_PATH=${PREFIX}/include
-        LIBRARY_PATH=${PREFIX}/lib
-
-        export LDFLAGS="-L${LIBRARY_PATH} $LDFLAGS"
-
-        cmake -LAH .. \
-              -DCMAKE_PREFIX_PATH=$PREFIX \
-              -DCMAKE_INSTALL_PREFIX=$PREFIX \
-              -DBOOST_ROOT=$PREFIX \
-              -DBOOST_INCLUDEDIR="${INCLUDE_PATH}" \
-              -DBOOST_LIBRARYDIR="${LIBRARY_PATH}" \
-
-              cmake --build . --config Release --target install
+        wget http://github.com/bbuchfink/diamond/releases/download/v0.7.10/diamond-linux64.tar.gz
+        tar xzf diamond-linux64.tar.gz
+        mv diamond $PREFIX/bin
         ;;
     Darwin)
         cd src
