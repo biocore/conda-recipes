f7f16b2fae3ef92f6e8e039f9a809f139bd2be62
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-02-22 12:13:42 -0800

    re-commit to avoid confusion with psipred

---
+++ recipes/blastlegacy/post-link.sh
@@ -0,0 +1,3 @@
+echo "Succesfully installed blast."
+echo "Now, either re-activate your environment or manually set environment variable BLASTMAT to $PREFIX/share/blast-2.2.26/data/."
+echo "In bash: export BLASTMAT=$PREFIX/share/blast-2.2.26/data/" > $PREFIX/.messages.txt
