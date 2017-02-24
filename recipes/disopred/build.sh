0e1fa1f2d1b1ad32f11a6f123f272a5e7df034a5
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-02-23 22:07:57 -0800

    removing sed from build.sh

--- recipes/disopred/build.sh
+++ recipes/disopred/build.sh
@@ -1,7 +1,3 @@
-sed -i "s|my $NCBI_DIR = \"\";|my \$NCBI_DIR = '/opt/anaconda1anaconda2anaconda3/bin/';|" run_disopred.pl
-sed -i "s|my \$EXE_DIR = abs_path(join '\/', \$dir, \"bin\");|my \$EXE_DIR = '/opt/anaconda1anaconda2anaconda3/bin/';|" run_disopred.pl
-sed -i "s|my \$DATA_DIR = abs_path(join '\/', \$dir,\"data\");|my \$DATA_DIR = '/opt/anaconda1anaconda2anaconda3/share/disopred_3.16/data/';|" run_disopred.pl
-sed -i "s|\$ENV{DSO_LIB_PATH} = join '\/', abs_path(\$dir), \"dso_lib\/\"|\$ENV{DSO_LIB_PATH} = '/opt/anaconda1anaconda2anaconda3/share/disopred_3.16/dso_lib/';|" run_disopred.pl
 rm -rf bin/
 cd src/
 make
