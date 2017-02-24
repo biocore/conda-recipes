927a23a99b005d344c4eee2770434b8b80f55eca
Stefan Janssen <stefan.m.janssen@gmail.com>
2017-02-24 13:36:49 -0800

    forgot to remove sed command from build.sh

--- recipes/memsat-svm/build.sh
+++ recipes/memsat-svm/build.sh
@@ -1,7 +1,3 @@
-sed -i "s|my \$ncbidir = '';|my \$ncbidir = '/opt/anaconda1anaconda2anaconda3/bin/';|" run_memsat-svm.pl
-sed -i "s|my \$mem_dir = '';|my \$mem_dir = '/opt/anaconda1anaconda2anaconda3/share/memsat-svm/';|" run_memsat-svm.pl
-sed -i "s|use lib 'lib';|use lib '/opt/anaconda1anaconda2anaconda3/share/memsat-svm/lib/';|" run_memsat-svm.pl
-
 make
 
 mkdir -p $PREFIX/bin
